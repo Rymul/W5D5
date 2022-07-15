675fe169be38037f354964c20dd0fc514cd82ef7
jefzli21 <jefzli21@gmail.com>
2022-07-15 14:28:43 -0700

    1st query done

--- jeffrey-ryan-movie-buff-long/movie_buff/01_queries.rb
+++ jeffrey-ryan-movie-buff-long/movie_buff/01_queries.rb
@@ -21,7 +21,7 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
-  Movie.select('movies.id, movies.title').left_outer_joins(:actors).where('name = ?, casting_id != ?', 'Harrison Ford', 1)
+  Movie.select('movies.id, movies.title').distinct.joins(:actors,:castings).where('actors.name = (?)', 'Harrison Ford').where.not('castings.ord = (?)', 1)
 end
 
 def biggest_cast
@@ -38,6 +38,7 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
+  Movie.select('movies.id, movies.title').group('movies.id').joins(:castings).order('COUNT(castings.actor_id) DESC').limit(3)
   
 end
 
@@ -55,6 +56,7 @@ def directed_by_one_of(them)
   # Find the id and title of all the movies directed by one of 'them'.
   
   # Note: Directors appear in the 'actors' table.
+  Movie.select('movies.id, movies.title').joins(:director).where('actors.name IN (?)', them)
 
 end
 
@@ -69,5 +71,5 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where('movies.yr < ?', 1940).pluck('movies.title')
 end
